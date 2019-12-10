37c664a2b26d7b506ad71aa0adcbe6cc916aec5b
Daniel Escobar <daescobarp@unal.edu.co>
2019-12-10 02:15:03 -0500

    Added Request Page

--- src/Screens/Request/Styled.js
+++ src/Screens/Request/Styled.js
@@ -6,22 +6,77 @@ export default styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  FlexView: {
+    flexDirection: 'row',
+  },
   MainCard: {
     backgroundColor: '#f7f7f8',
     margin: 10,
     padding: 10,
-    flexDirection: 'row',
   },
   ProfileCard: {
     borderColor: '#28323A',
     margin: 10,
     padding: 10,
-    flex: 2
+    flex: 2,
+    height: 'auto',
+    alignItems: 'center',
   },
   TaskCard: {
     borderColor: '#28323A',
     margin: 10,
     padding: 10,
-    flex: 3
+    flex: 3,
+  },
+  ButtonContainer: {
+    backgroundColor: '#28323A',
+    paddingVertical: 15,
+    marginHorizontal: 15,
+    flex: 2,
+    borderRadius: 10,
+  },
+  ButtonText: {
+    textAlign: 'center',
+    color: '#f7f7f8',
+    fontWeight: '700',
+  },
+  avatar: {
+    marginVertical: 20,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: '#28323A',
+    width: '100%',
+    height: '57%',
+  },
+  NameText: {
+    color: '#28323A',
+    fontWeight: '700',
+  },
+  TitleText: {
+    color: '#00b16a',
+    fontWeight: '700',
+  },
+  PriceText: {
+    color: '#28323A',
+    fontSize: 40,
+    fontWeight: '700',
+  },
+  PriceTitle: {
+    color: '#00b16a',
+    fontSize: 29,
+    fontWeight: '700',
+  },
+  OtherTitles1: {
+    color: '#00b16a',
+    fontWeight: '700',
+    fontSize: 15,
+    fontWeight: '700',
+  },
+  OtherTitles2: {
+    color: '#28323A',
+    fontWeight: '700',
+    fontSize: 15,
+    fontWeight: '700',
+    marginBottom: 15
   },
 });
