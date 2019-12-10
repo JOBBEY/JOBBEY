4699285f9f81f3c567ad587f504da6bcefa0f0b3
Daniel Escobar <daescobarp@unal.edu.co>
2019-12-09 22:29:31 -0500

    Updated packages and add scroll view

--- src/Screens/Categories/Categories.js
+++ src/Screens/Categories/Categories.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import NavigationService from '../../../Navigation/NavigationService'
 import styles from './Styled'
 import IconButton from './IconButton'
@@ -11,44 +11,46 @@ export default class Categories extends Component {
     }
     render() {
         return (
-            <View style={styles.container}>
-                <NavigationBar />
-                <Text
-                    style={styles.buttonText2}>
-                        ¿En qué categoría se encuentra tu solicitud?
-                </Text>
-                <View style={styles.columns}>
-                    <View
-                        style={styles.containerLeft}>
-                        <IconButton
-                            displayText='Computación'
-                            pngImage={require('../../assets/computer.png')} />
-                        <IconButton
-                            displayText='   Mecánica    '
-                            pngImage={require('../../assets/mechanic.png')}/>
-                        <IconButton
-                            displayText='     Legales      '
-                            pngImage={require('../../assets/balance.png')}/>
+            <ScrollView style={styles.container}>
+                <View style={styles.container}>
+                    <NavigationBar />
+                    <Text
+                        style={styles.buttonText2}>
+                            ¿En qué categoría se encuentra tu solicitud?
+                    </Text>
+                    <View style={styles.columns}>
+                        <View
+                            style={styles.containerLeft}>
+                            <IconButton
+                                displayText='Computación'
+                                pngImage={require('../../assets/computer.png')} />
+                            <IconButton
+                                displayText='Mecánica'
+                                pngImage={require('../../assets/mechanic.png')}/>
+                            <IconButton
+                                displayText='Legales'
+                                pngImage={require('../../assets/balance.png')}/>
+                        </View>
+                        <View
+                            style={styles.containerRigth}>
+                            <IconButton
+                                displayText='Plomería'
+                                pngImage={require('../../assets/pipeline.png')} />
+                            <IconButton
+                                displayText='Educación'
+                                pngImage={require('../../assets/education.png')}/>
+                            <IconButton
+                                displayText='Limpieza'
+                                pngImage={require('../../assets/cleaning.png')} />
+                        </View>
                     </View>
-                    <View
-                        style={styles.containerRigth}>
-                        <IconButton
-                            displayText='     Plomería    '
-                            pngImage={require('../../assets/pipeline.png')} />
-                        <IconButton
-                            displayText='   Educación   '
-                            pngImage={require('../../assets/education.png')}/>
-                        <IconButton
-                            displayText='    Limpieza    '
-                            pngImage={require('../../assets/cleaning.png')} />
-                    </View>
+                    <TouchableOpacity
+                        style={styles.buttonContainer}
+                        onPress={this.nextScreen}>
+                        <Text style={styles.buttonText}>Categoría no listada</Text>
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity
-                    style={styles.buttonContainer}
-                    onPress={this.nextScreen}>
-                    <Text style={styles.buttonText}>Categoría no listada</Text>
-                </TouchableOpacity>
-            </View>
+            </ScrollView>
         )
     }
 }
