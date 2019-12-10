867023560b39c526ae1c68b0c21cdace98a5431c
Daniel Escobar <daescobarp@unal.edu.co>
2019-12-10 00:37:06 -0500

    Login

--- Navigation/RootStackNavigator.js
+++ Navigation/RootStackNavigator.js
@@ -50,7 +50,7 @@ const routeConfig = {
     },
 };
 const navigatorConfig = {
-    initialRouteName: 'Request',
+    initialRouteName: 'Login',
     header: null,
     headerMode: "none",
     statusBarStyle: "light-content"
